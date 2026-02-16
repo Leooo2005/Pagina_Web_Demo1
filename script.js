52ad2a2f537fb18906fbd316e77896388ee948eb
Leonel_2005 <kevinlalangui2005@gmail.com>
2026-02-16 12:23:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
-function toggleMenu () {
+function toggleMenu() {
     const nav = document.getElementById("navMenu");
     nav.classList.toggle("active");
 }
 
-// --- FUNCIÓN PARA ABRIR LA TARJETA DEL PRODUCTO ---
 function abrirModal(nombre, precio, imagen, descripcion) {
-    // Llenamos la tarjeta oculta con los datos del producto al que se le dio clic
     document.getElementById('modalTitulo').innerText = nombre;
     document.getElementById('modalPrecio').innerText = "$" + precio;
     document.getElementById('modalImagen').src = imagen;
     document.getElementById('modalDescripcion').innerText = descripcion;
 
-    // MAGIA DE WHATSAPP:
-    // CAMBIA ESTE NÚMERO por el tuyo para probar (Usa 593 y tu número sin el 0)
+    // CAMBIA ESTE NÚMERO
     let numeroWhatsapp = "593999999999"; 
-    
-    // Armamos el mensaje automático
     let mensaje = "¡Hola! Me interesa pedir: " + nombre + " ($" + precio + ")";
     
-    // Convertimos el mensaje a formato de enlace de internet
     let enlaceFinal = "https://wa.me/" + numeroWhatsapp + "?text=" + encodeURIComponent(mensaje);
     
-    // Se lo pegamos al botón verde
     document.getElementById('botonWhatsapp').href = enlaceFinal;
-
-    // Mostramos la tarjeta en pantalla
     document.getElementById('modalProducto').classList.add('modal-activo');
 }
 
-// --- FUNCIÓN PARA CERRAR LA TARJETA ---
 function cerrarModal() {
     document.getElementById('modalProducto').classList.remove('modal-activo');
 }
